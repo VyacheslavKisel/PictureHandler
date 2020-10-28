6f1e5406887ea3c77ba02eace5564bb892c02d99
Kysil Viacheslav <viacheslav.kysil@gmail.com>
2020-10-28 16:17:27 +0200

    Initial commit

---
+++ PictureHandler/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PictureHandler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture.Location = new System.Drawing.Point(1, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(587, 451);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(594, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(600, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(225, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(600, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(139, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Made black-white";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(600, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Change image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 463);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picture);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PictureHandler/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureHandler
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPicture("smile.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            DialogResult dialogResult = openFileDialog1.ShowDialog();
+            LoadPicture(openFileDialog1.FileName);
+        }
+
+        private void LoadPicture(string filename)
+        {
+            try
+            {
+                bitmap = new Bitmap(Image.FromFile(filename));
+                textBox1.Text = openFileDialog1.FileName;
+                picture.Image = bitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ChangePicture();
+            MedianFiltering(bitmap);
+        }
+
+        private void ChangePicture()
+        {
+            Bitmap result = new Bitmap(bitmap);
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+                    if(checkBox1.Checked)
+                    {
+                        pixel = ChangeGrayScale(pixel);
+                    }
+                    result.SetPixel(x, y, pixel);
+                }
+            }
+            picture.Image = result;
+        }
+
+        private Color ChangeGrayScale(Color pixel)
+        {
+            int avg = (pixel.R + pixel.G + pixel.B + 1) / 3;
+            return Color.FromArgb(avg, avg, avg);
+        }
+
+        private void MedianFiltering(Bitmap bm)
+        {
+            var watch = Stopwatch.StartNew();
+            List<byte> termsList = new List<byte>();
+
+            byte[,] image = new byte[bm.Width, bm.Height];
+
+            //Convert to Grayscale
+            for (int i = 0; i < bm.Width; i++)
+            {
+                for (int j = 0; j < bm.Height; j++)
+                {
+                    var c = bm.GetPixel(i, j);
+                    byte gray = (byte)(.333 * c.R + .333 * c.G + .333 * c.B);
+                    image[i, j] = gray;
+                }
+            }
+
+            //applying Median Filtering 
+            for (int i = 0; i <= bm.Width - 3; i++)
+                for (int j = 0; j <= bm.Height - 3; j++)
+                {
+                    for (int x = i; x <= i + 2; x++)
+                        for (int y = j; y <= j + 2; y++)
+                        {
+                            termsList.Add(image[x, y]);
+                        }
+                    byte[] terms = termsList.ToArray();
+                    termsList.Clear();
+                    Array.Sort<byte>(terms);
+                    Array.Reverse(terms);
+                    byte color = terms[4];
+                    bm.SetPixel(i + 1, j + 1, Color.FromArgb(color, color, color));
+                }
+
+            picture.Image = bm;
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+        }
+    }
+}
